344e5ffdbc6a716f1869b7755b4d4214d5194e97
Cornelius Köpp <cornelius.koepp@fhdw.de>
2024-01-12 17:13:27 +0100

    (Draft) Cleanup Param-Extraction

--- utils/extract_params.py
+++ utils/extract_params.py
@@ -6,36 +6,25 @@ import json
 #
 
 
-def parse_xml_to_json2(xml_string):
-    root = ET.fromstring(xml_string)
-    result = {}
-    for param in root.findall('.//{http://knx.org/xml/project/20}Parameter'):
-        result[param.get('Name')] = param.get('Value')
-    # return json.dumps(result, indent=2)
-    return json.dumps(result)
-
-
-def parse_xml_to_json3(xml_string):
+def template_to_params(xml_string):
     root = ET.fromstring(xml_string)
     resultNames = []
     resultDefaults = []
-    # result = {"name":[], "default":[]}
-    result = {}
     for param in root.findall('.//{http://knx.org/xml/project/20}Parameter'):
         if param.get('Access') != 'None':
             resultNames.append(param.get('Name'))
             resultDefaults.append(param.get('Value'))
-            result[param.get('Name')] = param.get('Value')
-    # return json.dumps(result, indent=2)
     result = {"names": resultNames, "defaults": resultDefaults}
+    # return json.dumps(result, indent=2)
     return json.dumps(result)
 
 
 path = "lib/OFM-LogicModule/src/"
-template_file = path + "Logikmodul.templ.xml"
-output_file = path + "Logikmodul.templ.xml.params.json"
-with open(template_file, "r", encoding='utf-8') as file:
-    json_result = parse_xml_to_json3(file.read())
-    with open(output_file, "w", encoding='utf-8') as file2:
-        file2.write(json_result)
-        print('written to ' + output_file)
+filenameTemplate = path + "Logikmodul.templ.xml"
+filenameOutput = filenameTemplate + ".params.json"
+
+with open(filenameTemplate, "r", encoding='utf-8') as fileTemplate:
+    json_result = template_to_params(fileTemplate.read())
+    with open(filenameOutput, "w", encoding='utf-8') as fileOutput:
+        fileOutput.write(json_result)
+        print('written to ' + filenameOutput)
