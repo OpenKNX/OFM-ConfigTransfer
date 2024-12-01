3d4dc8c1e5eabeb3ada6106d15088fe74b47221e
Cornelius Köpp <cornelius.koepp@fhdw.de>
2024-12-01 22:27:12 +0100

    Tolerate Untrimmed Import-Strings
    * Draft: Tolerate Untrimmed Import-Strings
    * Cleanup Test JS
    * Fix: Do NOT Force Pattern for Import-Input

--- test/ConfigTransfer.test.js
+++ test/ConfigTransfer.test.js
@@ -729,7 +729,30 @@ describe('Button Handler', () => {
             expect(device.getParameterByName("CHN_Param4D").value).toBe("existing");
 
         });
-    
+
+        it("tolerates spaces around transfer-string", () => {
+            device.getParameterByName("UCTD_Import").value = " OpenKNX,cv1,0xAF42:0x23/CHN:0x18/3§;OpenKNX";
+            expect(() => uctBtnImport(device, online, progress, context)).not.toThrow(Error);
+
+            device.getParameterByName("UCTD_Import").value = " OpenKNX,cv1,0xAF42:0x23/CHN:0x18/3§;OpenKNX ";
+            expect(() => uctBtnImport(device, online, progress, context)).not.toThrow(Error);
+
+            device.getParameterByName("UCTD_Import").value = "OpenKNX,cv1,0xAF42:0x23/CHN:0x18/3§;OpenKNX ";
+            expect(() => uctBtnImport(device, online, progress, context)).not.toThrow(Error);
+
+            device.getParameterByName("UCTD_Import").value = "OpenKNX,cv1,0xAF42:0x23/CHN:0x18/3§;OpenKNX   ";
+            expect(() => uctBtnImport(device, online, progress, context)).not.toThrow(Error);
+
+            device.getParameterByName("UCTD_Import").value = "OpenKNX,cv1,0xAF42:0x23/CHN:0x18/3§;OpenKNX.";
+            expect(() => uctBtnImport(device, online, progress, context)).toThrow(Error);
+
+            device.getParameterByName("UCTD_Import").value = ".OpenKNX,cv1,0xAF42:0x23/CHN:0x18/3§;OpenKNX";
+            expect(() => uctBtnImport(device, online, progress, context)).toThrow(Error);
+
+            device.getParameterByName("UCTD_Import").value = ".OpenKNX,cv1,0xAF42:0x23/CHN:0x18/3§;OpenKNX.";
+            expect(() => uctBtnImport(device, online, progress, context)).toThrow(Error);
+        });
+
         it("fails on unsupported format version", () => {
             device.getParameterByName("UCT_Import").value = "OpenKNX,cv5,0xAF42:0x23/CHN:0x18/3§;OpenKNX";
             expect(() => uctBtnImport(device, online, progress, context)).toThrow(Error);
