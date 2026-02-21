10c3991cc0ff023531f8ea5fc5086ee06a877b20
Cornelius Köpp <cornelius.koepp@fhdw.de>
2026-02-21 17:49:14 +0100

    Add: Testcase "fails on planned multi-channel-format version"

--- test/ConfigTransfer.test.js
+++ test/ConfigTransfer.test.js
@@ -493,7 +493,17 @@ describe('Button Handler', () => {
             device.getParameterByName("UCTD_Import").value = "OpenKNX,cv12,0xAF42:0x23/CHN:0x18/3§;OpenKNX";
             expect(() => uctBtnImport(device, online, progress, context)).toThrow(/Format-Version NICHT unterstützt!/i);
         });
-    
+
+        it("fails with special error-message on planned multi-channel-format version", () => {
+            // next version
+            device.getParameterByName("UCTD_Import").value = "OpenKNX,cv1multi,0xAF42:0x23/CHN:0x18/3§;OpenKNX";
+            expect(() => uctBtnImport(device, online, progress, context)).toThrow(/Multi-Kanal Format-Version.* NICHT unterstützt!/i);
+
+            // wrong written next version
+            device.getParameterByName("UCTD_Import").value = "OpenKNX,cv1-multi,0xAF42:0x23/CHN:0x18/3§;OpenKNX";
+            expect(() => uctBtnImport(device, online, progress, context)).toThrow(/Format-Version NICHT unterstützt!/i);
+        });
+
         it("prints outputs and ignores comments", () => {
             device.getParameterByName("UCTD_Import").value = "OpenKNX,cv1,0xAF42:0x23/CHN:0x18/3§>echo1§#comment§>echo 2§;OpenKNX";
             device.getParameterByName("UCTD_Channel").value = 3;
